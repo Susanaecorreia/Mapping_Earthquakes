f0a10f65e6d1b9e57c6e4aa605aad72a43066f4b
Susanaecorreia <78492568+Susanaecorreia@users.noreply.github.com>
2021-05-20 12:58:38 -0400

    7days

--- Mapping_Lines/static/js/logicStep1.js
+++ Mapping_Lines/static/js/logicStep1.js
@@ -26,9 +26,9 @@ let baseMaps = {
 };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
-    layers: [satelliteStreets]
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -49,20 +49,25 @@ let myStyle = {
     weight: 2
 }
 
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data).addTo(map);
+});
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function (data) {
-    console.log(data);
-    //Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data, {
-    style: myStyle,
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-                + feature.properties.dst + "</h3>");
-        }
-})
-    .addTo(map);
-});
+// d3.json(torontoHoods).then(function(data) {
+//     console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data).addTo(map);
+    //   style:myStyle,
+    //   onEachFeature: function(feature, layer) {
+    //       layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+    //       +feature.properties.dst + "</h3>");
+    //   }
+    // })
+//   .addTo(map);
+//});
 
 // Grabbing our GeoJSON data.
 // d3.json(airportData).then(function (data) {
